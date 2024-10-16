ee8716b7e5f42dd75e6c08744e7c8bd9c6c2201f
mariohit <gilbertmario.hitimana@gmail.com>
2024-10-16 22:33:08 +0200

    erreur url

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // Importer HashRouter à la place de BrowserRouter
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
-)
+);
